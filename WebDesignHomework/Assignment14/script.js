81b0978ed2998a0c2f27ec8aa6f79ad97d17cc4d
Hayden Opitz <hayden.opitz@umontana.edu>
2025-05-04 22:05:03 -0600

    Update script.js

--- WebDesignHomework/Assignment14/script.js
+++ WebDesignHomework/Assignment14/script.js
@@ -1,12 +1,27 @@
-window.onload = function() {
-  const canvas = document.getElementById('pawCanvas');
-  const ctx = canvas.getContext('2d');
-  const img = new Image();
+window.onload = function () {
+  const canvas = document.getElementById("pawCanvas");
+  const ctx = canvas.getContext("2d");
+
   
+  function drawPaw(x, y) {
+    ctx.fillStyle = "#f4976c"; 
+    ctx.beginPath();
+    ctx.arc(x, y, 15, 0, Math.PI * 2, true); 
+    ctx.fill();
+    
+    const pawOffsets = [-20, 20];
+    pawOffsets.forEach(offset => {
+      ctx.beginPath();
+      ctx.arc(x + offset, y - 20, 10, 0, Math.PI * 2, true); 
+      ctx.fill();
+    });
+    pawOffsets.forEach(offset => {
+      ctx.beginPath();
+      ctx.arc(x + offset, y + 20, 10, 0, Math.PI * 2, true); 
+      ctx.fill();
+    });
+  }
+
   
-  img.onload = function() {
-    ctx.drawImage(img, 50, 50, 100, 100);
-  };
-  
-  img.src = 'images/paw-print.png'; 
+  drawPaw(150, 150);
 };
