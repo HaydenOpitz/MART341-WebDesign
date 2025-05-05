56a2c4c91b30479a416de520b81d4e46969e7282
Hayden Opitz <hayden.opitz@umontana.edu>
2025-05-04 22:29:25 -0600

    Update script.js

--- WebDesignHomework/Assignment14/script.js
+++ WebDesignHomework/Assignment14/script.js
@@ -1,53 +1,52 @@
 window.onload = function() {
-  const canvas = document.getElementById('pawCanvas');
-  const ctx = canvas.getContext('2d');
-
-  canvas.width = 300;
-  canvas.height = 300;
-
-  
-  function drawPaw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); 
+    const canvas = document.getElementById('pawCanvas');
+    const ctx = canvas.getContext('2d');
 
     
-    ctx.beginPath();
-    ctx.arc(150, 180, 50, 0, Math.PI * 2); 
-    ctx.fillStyle = '#4b4b4b'; 
-    ctx.fill();
-    ctx.closePath();
-
-   
-    const toeRadius = 20;
+    canvas.width = 300;
+    canvas.height = 300;
 
     
-    ctx.beginPath();
-    ctx.arc(110, 100, toeRadius, 0, Math.PI * 2); 
-    ctx.fillStyle = '#4b4b4b';
-    ctx.fill();
-    ctx.closePath();
+    function drawPaw() {
+        
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        
+        ctx.beginPath();
+        ctx.arc(150, 170, 60, 0, Math.PI * 2); 
+        ctx.fillStyle = '#000000'; 
+        ctx.fill();
+        ctx.closePath();
+
+       
+        ctx.beginPath();
+        ctx.arc(90, 120, 30, 0, Math.PI * 2);
+        ctx.fillStyle = '#000000';
+        ctx.fill();
+        ctx.closePath();
+
+        
+        ctx.beginPath();
+        ctx.arc(210, 120, 30, 0, Math.PI * 2);
+        ctx.fillStyle = '#000000';
+        ctx.fill();
+        ctx.closePath();
+
+        
+        ctx.beginPath();
+        ctx.arc(70, 200, 30, 0, Math.PI * 2);
+        ctx.fillStyle = '#000000';
+        ctx.fill();
+        ctx.closePath();
+
+        
+        ctx.beginPath();
+        ctx.arc(230, 200, 30, 0, Math.PI * 2);
+        ctx.fillStyle = '#000000';
+        ctx.fill();
+        ctx.closePath();
+    }
 
     
-    ctx.beginPath();
-    ctx.arc(190, 100, toeRadius, 0, Math.PI * 2); 
-    ctx.fillStyle = '#4b4b4b';
-    ctx.fill();
-    ctx.closePath();
-
-    
-    ctx.beginPath();
-    ctx.arc(80, 160, toeRadius, 0, Math.PI * 2); 
-    ctx.fillStyle = '#4b4b4b';
-    ctx.fill();
-    ctx.closePath();
-
-    
-    ctx.beginPath();
-    ctx.arc(220, 160, toeRadius, 0, Math.PI * 2); 
-    ctx.fillStyle = '#4b4b4b';
-    ctx.fill();
-    ctx.closePath();
-  }
-
- 
-  drawPaw();
-};
+    drawPaw();
+}
